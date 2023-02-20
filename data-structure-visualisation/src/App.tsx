6e6c468e9885afa81bdbbf395aefbd45665c5a56
Dawaad <jntucker02@gmail.com>
2023-02-20 13:52:44 +1100

    Initalised App using Vite

---
+++ data-structure-visualisation/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-red-300'>
+        hi
+    </div>
+  )
+}
+
+export default App
