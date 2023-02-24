ce7d5b6f016faea70cd2b7c5cea9b390120cf566
Dawaad <jntucker02@gmail.com>
2023-02-24 20:06:44 +1100

    Minor CSS + Edge Class Changes

--- data-structure-visualiser/src/Data-Structures/EdgeClass.ts
+++ data-structure-visualiser/src/Data-Structures/EdgeClass.ts
@@ -6,11 +6,13 @@ export class Edge<T>{
     vertex2: Vertex<T>
     weight: number
     directed:boolean
+    id: string
 
-    constructor(vertex1: Vertex<T>, vertex2: Vertex<T>, weight: number, directed:boolean){
+    constructor(vertex1: Vertex<T>, vertex2: Vertex<T>, weight: number, directed:boolean, id:string){
             this.vertex1 = vertex1;
             this.vertex2 = vertex2
             this.weight = weight
             this.directed = directed
+            this.id = id
     }
 }
